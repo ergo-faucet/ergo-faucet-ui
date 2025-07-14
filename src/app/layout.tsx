23b26abc8af20b03e5cf23275bda0ffaff2a1d72
Lucimad007 <smf007smf007@gmail.com>
2025-07-14 06:07:16 -0700

    add searchbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import { Roboto_Condensed } from "next/font/google";
 import { Providers } from "@/providers";
+import "./globals.css";
+
+const robotoCondensed = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Ergo Fuacet UI",
@@ -14,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`antialiased transition-colors`}>
+      <body
+        className={`antialiased transition-colors ${robotoCondensed.className}`}
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import Searchbar from "@/components/navbar/searchbar";
 import ToggleThemeButton from "@/components/navbar/toggle-theme-button";
 
 export default function Home() {
   // temp div for testing purposes
   return (
-    <div className="flex items-center justify-center h-screen bg-green-ergo-navbar">
+    <div className="flex items-center justify-center h-screen bg-white">
       <ToggleThemeButton />
+      <Searchbar />
     </div>
   );
 }
