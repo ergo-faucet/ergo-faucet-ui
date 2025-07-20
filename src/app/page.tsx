4d7bdb23c89fe36ff3e41c6e68085c8104951e05
Lucimad007 <smf007smf007@gmail.com>
2025-07-20 03:52:08 -0700

    add left navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Searchbar from '@/components/navbar/searchbar';
+import LeftNavbar from '@/components/navbar/left-navbar';
 import ToggleThemeButton from '@/components/navbar/toggle-theme-button';
 
 export default function Home() {
   // temp div for testing purposes
   return (
-    <div className='flex h-screen items-center justify-center bg-white'>
+    <div className='flex h-screen items-center justify-center gap-x-10 bg-amber-400'>
       <ToggleThemeButton />
-      <Searchbar />
+      <LeftNavbar />
     </div>
   );
 }
