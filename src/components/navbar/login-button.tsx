868afa1d31b11f376185fcf6c214b6239c6fb72d
Lucimad007 <smf007smf007@gmail.com>
2025-07-20 02:36:05 -0700

    run lint

--- src/components/navbar/login-button.tsx
+++ src/components/navbar/login-button.tsx
@@ -3,7 +3,11 @@ interface LoginButtonProps {
 }
 const LoginButton = ({ text }: LoginButtonProps) => {
   return (
-    <button className='w-32 h-10 font-bold text-lg rounded-xl border-[3px] border-beige-ergo-navbar dark:border-yellow-ergo-navbar   text-beige-ergo-navbar dark:text-yellow-ergo-navbar bg-green-ergo-navbar hover:bg-dark-green-ergo-navbar hover:transition-none transition-colors duration-300 ease-linear'>
+    <button
+      className='border-beige-ergo-navbar dark:border-yellow-ergo-navbar text-beige-ergo-navbar
+        dark:text-yellow-ergo-navbar bg-green-ergo-navbar hover:bg-dark-green-ergo-navbar h-10 w-32 rounded-xl
+        border-[3px] text-lg font-bold transition-colors duration-300 ease-linear hover:transition-none'
+    >
       {text}
     </button>
   );
