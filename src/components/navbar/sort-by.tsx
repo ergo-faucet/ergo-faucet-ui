b57251a96fca12a05099a81e8ec4cf5d8b0f5a07
Lucimad007 <smf007smf007@gmail.com>
2025-07-22 08:18:12 +0330

    fix spacings

--- src/components/navbar/sort-by.tsx
+++ src/components/navbar/sort-by.tsx
@@ -14,13 +14,13 @@ const SortBy = () => {
       {/* texts */}
       <div className='flex flex-col items-start justify-center space-y-0.5 pt-1 pl-[18px]'>
         <span className='text-[12px] text-gray-700 dark:text-gray-500'>Sort by</span>
-        <span className='text-[15px] text-black dark:text-white'>Release Date</span>
+        <span className='text-[15px] tracking-wider text-black dark:text-white'>Release Date</span>
       </div>
 
       {/* button and vertical line */}
       <button
         className='relative mr-2 h-full w-auto bg-transparent pl-4 text-gray-600 before:absolute before:top-1/2
-          before:left-2 before:h-9 before:w-px before:-translate-y-1/2 before:bg-gray-600'
+          before:left-3 before:h-9 before:w-px before:-translate-y-1/2 before:bg-gray-600'
       >
         <LuArrowUpNarrowWide size={30} />
       </button>
