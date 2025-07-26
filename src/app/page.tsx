75d2995d37453c2e0f834ab555cc8d0ccc857371
Lucimad007 <smf007smf007@gmail.com>
2025-07-26 20:06:58 +0330

    add shadcn dropdown menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import SortBy from '@/components/navbar/sort-by';
 import ToggleThemeButton from '@/components/navbar/toggle-theme-button';
-import AuthTask from '@/components/package-details/auth-task';
 
 export default function Home() {
   // temp div for testing purposes
