8a33b38104a916cee73d0bb0a15fa8fdaaa2e965
Lucimad007 <smf007smf007@gmail.com>
2025-07-27 19:27:37 +0330

    add wallet components & show them in connect wallet sidebar

--- src/components/wallet-sidebar/connect-wallet-sidebar.tsx
+++ src/components/wallet-sidebar/connect-wallet-sidebar.tsx
@@ -2,6 +2,8 @@ import { Volkhov } from 'next/font/google';
 import { IoMdClose } from 'react-icons/io';
 import { IoWalletSharp } from 'react-icons/io5';
 
+import Wallet from './wallet';
+
 const volkhov = Volkhov({
   subsets: ['latin'],
   weight: ['400'],
@@ -12,7 +14,7 @@ const ConnectWalletSidebar = () => {
     // container
     <div
       className={`${volkhov.className} dark:bg-gray-1200 relative flex h-full w-[452px] flex-col items-center
-        justify-center bg-[#F8F8F8]`}
+        justify-center space-y-21 bg-[#F8F8F8]`}
     >
       {/* close button */}
       <button className='absolute top-2 left-2 cursor-pointer'>
@@ -25,10 +27,18 @@ const ConnectWalletSidebar = () => {
         <span className='text-[22px]'>Connect your wallet</span>
       </div>
 
+      {/* wallets */}
+      <div className='flex flex-col space-y-2'>
+        {/* Natilus */}
+        <Wallet src='/icons/natilus-40x40.png' alt='Natilus icon' size={40} name='Natilus' selected={true} />
+        {/* Ergo Pay */}
+        <Wallet alt='Ergo Pay icon' size={40} name='Ergo Pay' selected={false} />
+      </div>
+
       {/* connect button */}
       <button
-        className='h-11 w-25 rounded-xl border border-green-400 bg-green-700 text-[17px] tracking-wider text-white
-          shadow-md/15 shadow-black dark:shadow-white'
+        className='h-11 w-25 cursor-pointer rounded-xl border border-green-400 bg-green-700 text-[17px] tracking-wider
+          text-white shadow-md/15 shadow-black hover:bg-green-900 dark:shadow-white'
       >
         Connect
       </button>
---
+++ src/components/wallet-sidebar/wallet.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+
+import { cn } from '@/lib/utils';
+
+// Assuming you're using a utility like `clsx` or `tailwind-merge`
+
+interface WalletProps {
+  name: string;
+  src?: string;
+  alt: string;
+  size: number;
+  selected: boolean;
+}
+
+const Wallet = ({ name, src, alt, size, selected }: WalletProps) => {
+  return (
+    <div
+      className={cn(
+        'flex h-14 w-70 items-center justify-between rounded-[19px] border bg-transparent p-2 pr-3',
+        selected ? 'border-green-400' : 'border-gray-600',
+      )}
+    >
+      {/* Icon and name */}
+      <div className='flex items-center justify-center space-x-2'>
+        {src ? (
+          <Image src={src} alt={alt} width={size} height={size} />
+        ) : (
+          <div
+            className={cn('rounded-full', selected ? 'bg-gray-500' : 'bg-[#6D6D6D]')}
+            style={{ width: `${size}px`, height: `${size}px` }}
+          />
+        )}
+        <span className='text-[17px] tracking-wider text-black dark:text-white'>{name}</span>
+      </div>
+
+      {/* The circle */}
+      <div
+        className={cn(
+          'h-5 w-5 rounded-full border',
+          selected ? 'bg-green-1000 border-green-400' : 'border-gray-500 bg-gray-500',
+        )}
+      />
+    </div>
+  );
+};
+
+export default Wallet;
