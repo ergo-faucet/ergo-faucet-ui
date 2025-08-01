cea7065a3b46942398fdec46d09fdf34d58acd26
Lucimad007 <smf007smf007@gmail.com>
2025-08-02 01:36:13 +0330

    add shadcn dropdown menu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import LeftNavbar from '@/components/navbar/left-navbar';
+import Searchbar from '@/components/navbar/searchbar';
 import ToggleThemeButton from '@/components/navbar/toggle-theme-button';
-import AuthTask from '@/components/package-details/auth-task';
 
 export default function Home() {
   // temp div for testing purposes
   return (
     <div className='flex h-screen items-center justify-center gap-x-10 bg-amber-400'>
       <ToggleThemeButton />
-      <LeftNavbar />
+      <Searchbar />
     </div>
   );
 }
