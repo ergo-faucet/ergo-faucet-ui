f60751c7abc40324f2490c9bdac00cd5ce2749b8
Lucimad007 <smf007smf007@gmail.com>
2025-08-13 11:26:47 +0330

    update generate icon avatar & clear page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import ToggleThemeButton from '@/components/navbar/toggle-theme-button';
-import ConnectWalletSidebar from '@/components/wallet-sidebar/connect-wallet-sidebar';
-
 export default function Home() {
-  // temp div for testing purposes
-  return (
-    <div className='flex h-screen items-center justify-center gap-x-10 bg-amber-400'>
-      <ToggleThemeButton />
-      <ConnectWalletSidebar />
-    </div>
-  );
+  return <></>;
 }
