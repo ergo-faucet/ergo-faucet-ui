d9dcfe7369fe0b2dd5bef4f87cc1f23492f4a74d
Lucimad007 <smf007smf007@gmail.com>
2025-08-13 10:15:03 +0330

    clear page.tsx and fix generate auth icon component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import SortBy from '@/components/navbar/sort-by';
-import ToggleThemeButton from '@/components/navbar/toggle-theme-button';
-
 export default function Home() {
-  // temp div for testing purposes
-  return (
-    <div className='flex h-screen items-center justify-center space-x-5 bg-white dark:bg-black'>
-      <ToggleThemeButton />
-      <SortBy />
-    </div>
-  );
+  return <></>;
 }
