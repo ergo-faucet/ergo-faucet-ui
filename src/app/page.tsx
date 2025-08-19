86fa61903cd7c1e2d2dc98f8ad7435fc2dfabaa0
Lucimad007 <smf007smf007@gmail.com>
2025-08-19 14:49:06 +0330

    clear page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-import AssetTable from '@/components/claim-modal/asset-details';
-import ToggleThemeButton from '@/components/navbar/toggle-theme-button';
-import { Asset } from '@/types';
-
-const assets: Asset[] = [
-  { amount: 12n, decimal: 1, name: 'Ergo token', tokenId: 'fioairijfafajnfs' },
-  { amount: 50n, decimal: 3, name: 'Ergo token2', tokenId: 'foijafj9jf9afuafsmfiofajfj0jf0ja0idadomadjsuf09a0' },
-];
 export default function Home() {
-  // temp div for testing purposes
-  return (
-    <div className='flex h-screen items-center justify-center space-x-5 bg-white dark:bg-black'>
-      <ToggleThemeButton />
-      <AssetTable assets={assets} />
-    </div>
-  );
+  return <></>;
 }
