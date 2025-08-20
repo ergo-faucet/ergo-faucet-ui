0ed07a5bbbcb88a5c069f1610c3d3080a172790f
Lucimad007 <smf007smf007@gmail.com>
2025-08-20 13:46:37 +0330

    wrap dropdown menu components to avoid changing left empty space when adding/removing filters

--- src/components/navbar/searchbar/searchbar-input.tsx
+++ src/components/navbar/searchbar/searchbar-input.tsx
@@ -12,39 +12,41 @@ const SearchbarInput = () => {
 
   return (
     <DropdownMenu>
-      {/* filter components */}
-      {filterComponents.map((FilterComponent, id) => {
-        return <React.Fragment key={id}>{FilterComponent.element}</React.Fragment>;
-      })}
+      <div className='flex items-center'>
+        {/* filter components */}
+        {filterComponents.map((FilterComponent, id) => {
+          return <React.Fragment key={id}>{FilterComponent.element}</React.Fragment>;
+        })}
 
-      <DropdownMenuTrigger className='flex items-center' asChild>
-        <input
-          type='text'
-          placeholder='Search'
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              e.preventDefault(); // stop form submit
-              e.stopPropagation(); // stop Radix from toggling menu
-              addHistory(searchQuery);
-              setSearchQuery('');
-            }
-          }}
-          className='mx-3 w-full text-[18px] font-medium tracking-wide text-gray-700 placeholder-gray-700
-            focus:outline-none dark:text-gray-500 dark:placeholder-gray-500'
-        />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        className='dark:bg-gray-1000 mt-3 w-56 rounded-[8px] border border-gray-800 bg-gray-100 dark:border-gray-600
-          [&_*]:text-black [&_*]:dark:text-white [&>*]:hover:bg-gray-300 [&>*]:hover:dark:bg-gray-800'
-        align='start'
-      >
-        {/* steps */}
-        <Step1 />
-        <Step2 />
-        <Step3 />
-      </DropdownMenuContent>
+        <DropdownMenuTrigger className='flex items-center' asChild>
+          <input
+            type='text'
+            placeholder='Search'
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                e.preventDefault(); // stop form submit
+                e.stopPropagation(); // stop Radix from toggling menu
+                addHistory(searchQuery);
+                setSearchQuery('');
+              }
+            }}
+            className='mx-3 w-full text-[18px] font-medium tracking-wide text-gray-700 placeholder-gray-700
+              focus:outline-none dark:text-gray-500 dark:placeholder-gray-500'
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className='dark:bg-gray-1000 mt-3 w-56 rounded-[8px] border border-gray-800 bg-gray-100 dark:border-gray-600
+            [&_*]:text-black [&_*]:dark:text-white [&>*]:hover:bg-gray-300 [&>*]:hover:dark:bg-gray-800'
+          align='start'
+        >
+          {/* steps */}
+          <Step1 />
+          <Step2 />
+          <Step3 />
+        </DropdownMenuContent>
+      </div>
     </DropdownMenu>
   );
 };
