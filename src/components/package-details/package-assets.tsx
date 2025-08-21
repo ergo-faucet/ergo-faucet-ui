693ef20c60fd0078ce28b643623941cc744064b3
Lucimad007 <smf007smf007@gmail.com>
2025-08-21 15:30:47 +0330

    fix dark after pseudo element color bug

--- src/components/package-details/package-assets.tsx
+++ src/components/package-details/package-assets.tsx
@@ -11,7 +11,7 @@ const PackageAssets = ({ assets }: PackageAssetsProps) => {
     <div className='flex h-auto w-[286px] flex-col items-start justify-start'>
       <div
         className='relative mb-2 flex h-full w-full items-start justify-start text-gray-900 after:mt-3 after:ml-3
-          after:block after:h-0.5 after:w-[100%] after:bg-gray-900 dark:text-gray-200 after:dark:bg-gray-200'
+          after:block after:h-0.5 after:w-[100%] after:bg-gray-900 dark:text-gray-200 dark:after:bg-gray-200'
       >
         <span className='text-[15px] font-extrabold'>Assets</span>
       </div>
