a5d1227a7ba5483400f6e67d5c3d729d980b9a0e
Lucimad007 <smf007smf007@gmail.com>
2025-08-24 04:57:30 +0330

    remove default close button of sheet component in shadcn

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -41,8 +41,10 @@ function SheetContent({
   className,
   children,
   side = 'right',
+  defaultCloseButton,
   ...props
 }: React.ComponentProps<typeof SheetPrimitive.Content> & {
+  defaultCloseButton?: boolean;
   side?: 'top' | 'right' | 'bottom' | 'left';
 }) {
   return (
@@ -69,14 +71,16 @@ function SheetContent({
         {...props}
       >
         {children}
-        <SheetPrimitive.Close
-          className='ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4
-            rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2
-            focus:outline-hidden disabled:pointer-events-none'
-        >
-          <XIcon className='size-4' />
-          <span className='sr-only'>Close</span>
-        </SheetPrimitive.Close>
+        {defaultCloseButton && (
+          <SheetPrimitive.Close
+            className='ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4
+              rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2
+              focus:outline-hidden disabled:pointer-events-none'
+          >
+            <XIcon className='size-4' />
+            <span className='sr-only'>Close</span>
+          </SheetPrimitive.Close>
+        )}
       </SheetPrimitive.Content>
     </SheetPortal>
   );
