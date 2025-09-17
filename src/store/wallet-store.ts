10bbd38c3a79bf8558b1af0988dfff3ceb7df836
Lucimad007 <smf007smf007@gmail.com>
2025-09-17 11:52:02 +0330

    make wallet address in memory - just like access token

--- src/store/wallet-store.ts
+++ src/store/wallet-store.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 
 interface WalletState {
   address: string;
@@ -8,14 +7,9 @@ interface WalletState {
   disconnect: () => void;
 }
 
-export const useWalletStore = create<WalletState>()(
-  persist(
-    (set) => ({
-      address: '',
-      connected: false,
-      setAddress: (address) => set({ address, connected: Boolean(address) }),
-      disconnect: () => set({ address: '', connected: false }),
-    }),
-    { name: 'wallet-storage' }, // in localStorage
-  ),
-);
+export const useWalletStore = create<WalletState>()((set) => ({
+  address: '',
+  connected: false,
+  setAddress: (address) => set({ address, connected: Boolean(address) }),
+  disconnect: () => set({ address: '', connected: false }),
+}));
