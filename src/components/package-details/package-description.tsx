6a888d4d6d5766b4a9ecf2bbe06a8f7fc75d0633
Lucimad007 <smf007smf007@gmail.com>
2025-10-30 17:09:36 +0330

    use scroll area for long descriptions

--- src/components/package-details/package-description.tsx
+++ src/components/package-details/package-description.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { inter } from '@/fonts';
 import { cn } from '@/lib';
 
@@ -7,11 +8,27 @@ interface PackageDescriptionProps {
 }
 
 const PackageDescription = ({ info, className }: PackageDescriptionProps) => {
+  const isScrollable = info.length > 250;
+
   return (
     <div className={cn('flex min-h-[70px] w-full flex-col items-start justify-start gap-2', className)}>
       <span className='text-[15px] font-extrabold text-gray-700 dark:text-gray-200'>Description</span>
+
       <div className='min-h-[45px] w-full rounded-[5px] bg-gray-200 text-black dark:bg-gray-900 dark:text-white'>
-        <div className={`${inter.className} mx-2 my-1.5 text-[10px]`}>{info}</div>
+        {isScrollable ? (
+          <ScrollArea
+            className={cn(
+              'h-24 w-full rounded-[5px]',
+              // custom scrollbar colors
+              '[&_[data-orientation=vertical]_>div]:bg-gray-700',
+              'dark:[&_[data-orientation=vertical]_>div]:bg-gray-300',
+            )}
+          >
+            <div className={`${inter.className} mx-2 my-1.5 pr-2 text-[10px]`}>{info}</div>
+          </ScrollArea>
+        ) : (
+          <div className={`${inter.className} mx-2 my-1.5 text-[10px]`}>{info}</div>
+        )}
       </div>
     </div>
   );
