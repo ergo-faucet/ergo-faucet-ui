a47c5b9ddf4a0cabdf6b3d059e1c72388bbd5843
Lucimad007 <smf007smf007@gmail.com>
2025-10-30 17:29:25 +0330

    add scroll area for assets in the package details

--- src/components/package-details/package-assets.tsx
+++ src/components/package-details/package-assets.tsx
@@ -1,3 +1,4 @@
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { Asset } from '@/types';
 
 import { LineAfterText } from './line-after-text';
@@ -11,9 +12,19 @@ const PackageAssets = ({ assets }: PackageAssetsProps) => {
   return (
     <div className='flex h-auto w-[286px] flex-col items-start justify-start'>
       <LineAfterText text='Assets' />
-      {assets.map((asset, idx) => {
-        return <TokenDetails key={idx} asset={asset} />;
-      })}
+
+      {/* scrollable list */}
+      <ScrollArea
+        className='mt-1 h-28 w-full rounded-md [&_[data-orientation=vertical]_>div]:bg-gray-700
+          dark:[&_[data-orientation=vertical]_>div]:bg-gray-300'
+      >
+        {/* internal padding so scrollbar never overlaps content */}
+        <div className='flex w-full flex-col gap-1 pr-4 pl-[2px]'>
+          {assets.map((asset, idx) => (
+            <TokenDetails key={idx} asset={asset} />
+          ))}
+        </div>
+      </ScrollArea>
     </div>
   );
 };
