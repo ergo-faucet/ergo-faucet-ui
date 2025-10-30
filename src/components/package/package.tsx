ad63fa1f84844e67ff66a8f4cb341eb445d7a2f8
Lucimad007 <smf007smf007@gmail.com>
2025-10-30 15:19:42 +0330

    fix package component overflows

--- src/components/package/package.tsx
+++ src/components/package/package.tsx
@@ -18,28 +18,32 @@ const Package = ({ title, assets, authTypes, startDate, endDate, onClick }: Pack
     // container
     <div
       onClick={onClick}
-      className={`${robotoCondensed.className} dark:bg-gray-1000 flex h-[195px] min-w-[250px] flex-col rounded-[18px]
-        border border-gray-500 bg-gray-100 shadow-[-1px_3px_10px_rgba(0,0,0,0.5)] dark:border-gray-700`}
+      className={`${robotoCondensed.className} dark:bg-gray-1000 flex h-[195px] max-w-full min-w-[250px] flex-col
+        overflow-hidden rounded-[18px] border border-gray-500 bg-gray-100 shadow-[-1px_3px_10px_rgba(0,0,0,0.5)]
+        transition-transform hover:scale-[1.01] dark:border-gray-700`}
     >
       {/* title */}
       <div
-        className={`${volkhov.className} mt-4 ml-6 min-h-[51px] max-w-[250px] text-[20px] tracking-wider text-gray-900
+        className={`${volkhov.className} mt-4 min-h-[51px] truncate px-6 text-[20px] tracking-wider text-gray-900
           dark:text-gray-200`}
+        title={title}
       >
         {title}
       </div>
 
       {/* assets */}
-      <div className='mt-2 ml-4 flex h-8 w-full items-center justify-between pr-7'>
-        <span className='text-[14px] font-bold text-gray-700 dark:text-gray-600'>Assets in package</span>
-        <Avatars className='ml-6 self-end' assets={assets} />
+      <div className='mt-2 flex h-8 w-full items-center justify-between overflow-hidden px-6'>
+        <span className='truncate text-[14px] font-bold text-gray-700 dark:text-gray-600'>Assets in package</span>
+        <Avatars className='ml-3 shrink-0 self-end' assets={assets} />
       </div>
 
       {/* auth methods */}
-      <span className='mt-2 ml-4 text-gray-800 dark:text-[#A4A4A4]'>{authTypes.length} Auth methods/task required</span>
+      <span className='mt-2 truncate px-6 text-gray-800 dark:text-[#A4A4A4]'>
+        {authTypes.length} Auth methods/task required
+      </span>
 
       {/* deadline */}
-      <Deadline startDate={startDate} endDate={endDate} className='mt-4 ml-4 pr-8' />
+      <Deadline startDate={startDate} endDate={endDate} className='mt-auto mb-3 truncate px-6' />
     </div>
   );
 };
